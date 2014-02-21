e79dbd26feee82582e8a6b8aedd641710b7e37a1
Ben Zhang <legendben@gmail.com>
2014-02-21 00:08:11 -0800

    refactor

--- sudoku_game.rb
+++ sudoku_game.rb
@@ -1,84 +1,86 @@
+require 'rubygems'
+require 'colorize'
+
 class SudokuGame
   attr_accessor :tiles
-
-  @@time = 0
-
-  def initialize(input=nil)
+  def initialize(input = nil)
     @tiles = []
-
     if input
       input = input.split(" ").map{|x| x.split("")}
-
+      if input.map(&:count) != [9]*9
+        raise "input is wrong"
+      end
       build_tiles(input)
+      puts "\nGiven Sudoku Puzzle:\n\n"
       pretty_print
     end
   end
 
   def solve
-    total_value_before = total_value
-    total_value_after = nil
-    while not_solved? #&& total_value_before != total_value_after
-      total_value_before = total_value
+    run_elimination_algorithm
+    if solved?
+      puts "Solved!!!"
+      pretty_print
+      return
+    else
+      p "make best guess"
+      make_best_guess
+    end
+  end
 
+  def run_elimination_algorithm
+    loop do
+      puts solved_tile_count_before = solved_tile_count
       @tiles.flatten.each do |tile|
-        # p tile
         unless tile.value
           tile.update_candidates
           tile.set_value
         end
-        # p tile
       end
+      pretty_print
+      puts solved_tile_count_after = solved_tile_count
 
-      total_value_after = total_value
+      break if solved? || solved_tile_count_before == solved_tile_count_after
     end
-
-    pretty_print
-    # if !not_solved?
-    #   p "solved!!!"
-    #   pretty_print
-    #   return
-    # elsif total_value_after == total_value_before
-    #   p "tried setting value, now trying different games"
-    #   p tile_to_try
-    #   p games_to_try: games_to_try.count
-
-    #   games_to_try.each(&:pretty_print)
-
-    #   games_to_try.each do |game|
-    #     puts "TRY A subgame #{@@time += 1}"
-    #     game.pretty_print
-    #     # sleep 1
-    #     game.solve
-    #   end
-    # end
   end
 
-  def not_solved?
+  def make_best_guess
+    x = tile_to_try.x
+    y = tile_to_try.y
+    candidates = tile_to_try.candidates
+
+    games_to_try.each_with_index do |game, i|
+      puts "Solving sub-puzzle: Row #{x} Column #{y} Guess: #{candidates[i]}"
+      game.pretty_print
+      return game.solve
+    end
+  end
+
+  def unsolved?
     @tiles.flatten.any? { |tile| tile.value.nil? }
   end
 
+  def solved?
+    @tiles.flatten.all? { |tile| !tile.value.nil? }
+  end
+
+  def unsolved_tiles
+    @tiles.flatten.select{|x| x.value.nil? }
+  end
+
   def tile_to_try
-    @tiles_to_try ||= @tiles.flatten.select{|x| x.value.nil? }.sort{|x, y| x.candidates.length <=> y.candidates.length }.first
+    unsolved_tiles.sort{|x, y| x.candidates.length <=> y.candidates.length }.first
   end
 
   def games_to_try
-    tile = tile_to_try
-
-    @games_to_try ||= tile.candidates.map do |num|
+    @games_to_try ||= tile_to_try.candidates.map do |num|
       game = Marshal.load(Marshal.dump(self))
-
-      game.tiles[tile.x-1][tile.y-1].set_value(num)
+      game.tiles[tile_to_try.x-1][tile_to_try.y-1].set_value(num)
       game
     end
   end
 
-  def output
-    @tiles.map do |row|
-      row.map(&:serialize).join
-    end.join(" ")
-  end
-
-  def total_value
+  def solved_tile_count
     @tiles.flatten.map(&:value).compact.count
   end
 
@@ -92,33 +94,25 @@ class SudokuGame
       (0..8).map do |y|
         value = input[x][y]
         if value == "."
-          Tile.new(self, x, y, nil, false)
+          Tile.new(self, x+1, y+1, nil, false)
         else
-          Tile.new(self, x, y, value.to_i, true)
+          Tile.new(self, x+1, y+1, value.to_i, true)
         end
       end
     end
   end
 
   def pretty_print
-    require 'rubygems'
-    require 'colorize'
-
     @tiles.each_with_index do |row, i|
       row.each_with_index do |tile, j|
-        if tile.given
-          print(tile.value ? tile.value.to_s.red : "*")
-        else
-          print(tile.value ? tile.value.to_s.green : "*")
-        end
-        putc " "
-        putc "|" if j %3 == 2
+        color = tile.given ? :red : :green
+        print tile.value ? tile.value.to_s.send(color) : "*"
+        putc " "; putc "|" if j %3 == 2
       end
-      puts
-      puts "-"*22 if i % 3 == 2
+      puts; puts "-"*22 if i % 3 == 2
     end
-    puts total_value
-    puts
+
+    puts "Tiles remaining to be solved: #{81 - solved_tile_count}\n\n"
   end
 end
 
@@ -134,14 +128,6 @@ class Tile
     @given = given
   end
 
-  def to_s
-    {x: y, y: y, value: value, given: given, candidates: @candidates}
-  end
-
-  def serialize
-    value ? value.to_s : "."
-  end
-
   def set_value(value=nil)
     if value
       @value = value
@@ -150,81 +136,47 @@ class Tile
       if candidates.count == 1
         @value = candidates.first
         @candidates = []
-      # elsif derived_number
-      #   @value = derived_number
-      #   @candidates = []
+      elsif derived_number
+        @value = derived_number
+        @candidates = []
       end
     end
   end
 
   def derived_number
-    x_difference = candidates - x_candidates
-    y_difference = candidates - y_candidates
-    z_difference = candidates - z_candidates
-
-    [x_difference, y_difference, z_difference].each do |diff|
-      if diff.count == 1
-        return diff[0]
-      else
-        return nil
-      end
+    [x_neighbors, y_neighbors, z_neighbors].each do |neighbors|
+      neighbor_candidates = (neighbors - [self]).map(&:candidates).flatten
+      exclusive_candidates = candidates - neighbor_candidates
+      return exclusive_candidates[0] if exclusive_candidates.count == 1
     end
+    nil
   end
 
   def candidates
-    return [] if @value
-
-    @candidates ||= range - (x_occupied | y_occupied | z_occupied)
+    @value ? [] : (@candidates || update_candidates)
   end
 
   def update_candidates
-    @candidates = nil
-    candidates
+    impossible_values = x_neighbors.map(&:value) | y_neighbors.map(&:value) | z_neighbors.map(&:value)
+    @candidates = (1..9).to_a - impossible_values
   end
 
-  def x_occupied
-    @game.tiles[x-1].map(&:value)
+  def x_neighbors
+    @game.tiles[x-1]
   end
 
-  def y_occupied
-    @game.tiles.map{ |tiles| tiles[y] }.map(&:value)
+  def y_neighbors
+    @game.tiles.map{ |tiles| tiles[y-1] }
   end
 
-  def z_occupied
-    x1 = x - x % 3
-    y1 = y - y % 3
-    (x1..x1+2).map do |x|
-      (y1..y1+2).map do |y|
-        @game.tiles[x][y].value
-      end
-    end.flatten
-  end
-
-  def x_candidates
-    (@game.tiles[x] - [self]).map(&:candidates).flatten.uniq
-  end
-
-  def y_candidates
-    (@game.tiles.map{ |tiles| tiles[y] }.flatten - [self]).map(&:candidates).flatten.uniq
-  end
-
-  def z_candidates
-    x1 = x - x % 3
-    y1 = y - y % 3
-
-    ((x1..x1+2).map do |x|
-      (y1..y1+2).map do |y|
-        @game.tiles[x][y]
-      end
-    end.flatten - [self]).map(&:candidates).flatten.uniq
-  end
-
-  def range
-    (1..9).to_a
+  def z_neighbors
+    x1 = ((x - 1) / 3 ) * 3
+    y1 = ((y - 1) / 3 ) * 3
+    (x1..x1+2).map { |x| (y1..y1+2).map { |y| @game.tiles[x][y] } }.flatten
   end
 end
 
-#game = SudokuGame.new("586374912 137952864 249816573 872543196 693781245 415629738 954237681 721468359 368195427")
-game = SudokuGame.new("..6.7.9.. 1..9....4 ...8..5.3 87.5...9. ..37.1... 4...2.7.8 .5.2..6.. 7....8... 3..1..4..")
-#game = SudokuGame.new("..6.7.9.. 1..9....4 ...8..5.3 87.5...9. ..37.1... 4...2.7.8 .5.2..6.. 7....8... 3..1..4..")
+# game = SudokuGame.new("586374912 137952864 249816573 872543196 693781245 415629738 954237681 721468359 368195427")
+# game = SudokuGame.new("..6.7.9.. 1..9....4 ...8..5.3 .7.5...9. ..37.1... 4...2.7.8 .5.2..6.. 7....8... 3..1..4..")
+game = SudokuGame.new("61.....8. .2.53..9. ..3...6.. ..94..... 4..1.7..9 .....98.. ..4...1.. .5..21.3. .3.....76")
 game.solve
